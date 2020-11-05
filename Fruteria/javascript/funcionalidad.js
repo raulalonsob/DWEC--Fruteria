21e476b000f176ad49f8f669c0e440bec6c4d4c4
Raul Alonso <raul@raul>
2020-11-05 18:52:30 +0100

    Inicio de la version 2. Creación de clases y objetos.

--- Fruteria/javascript/funcionalidad.js
+++ Fruteria/javascript/funcionalidad.js
@@ -1,8 +1,59 @@
 /**Arrays donde se guardaran los datos*/
-
-var frutas=["melon","manzana","pera","frambuesa","pitaya","arandano","platano","papaya","fresa","melocoton"];
+/**var frutas=["melon","manzana","pera","frambuesa","pitaya","arandano","platano","papaya","fresa","melocoton"];
 var precios=[0.74, 1.69, 1.59, 12.74, 7.54,7.15, 2.15,5.35,5.74,1.40];
 var kilos=[0,0,0,0,0,0,0,0,0,0];
+ */
+
+
+ /**
+  * Clase principal de Fruta
+  */
+ class Fruta {
+    constructor(nombre, kg, precio) {
+        this.nombre = nombre;
+        this.kg = kg;
+        this.precio = precio;
+    }
+
+
+}
+/**
+ * Clase hija de la clase Fruta que añade los atributos de verano
+ */
+class FrutaVerano extends Fruta {
+    constructor(nombre, kg, precio, proximidad, region) {
+        super(nombre, kg, precio);
+        this.proximidad = proximidad;
+        this.region = region;
+    }
+}
+
+
+/**
+ * Clase hija de la clase Fruta que añade los atributos de invierno
+ */
+class FrutaInvierno extends Fruta {
+    constructor(nombre, kg, precio, conservar) {
+        super(nombre, kg, precio);
+        this.conservar = conservar;
+    }
+
+}
+
+/**
+ * Objetos donde se almacena la información de cada fruta
+ */
+var melon =new FrutaVerano("melon",0,0.74,"es de proximidad","Cáceres");
+var manzana = new FrutaInvierno("manzana",0,1.69,"se recomienda");
+var pera = new FrutaInvierno("pera",0,1.59,"no se recomienda");
+var frambuesa = new FrutaVerano("frambuesa",0,1.59,"no es de proximidad","Huelva");
+var pitaya = new FrutaInvierno("pitaya",0,7.54,"se recomienda");
+var arandano = new FrutaVerano("arandano",0,7.15,"no es de proximidad","Huelva");
+var platano = new FrutaInvierno("platano",0,2.15,"no se recomienda");
+var papaya = new FrutaVerano("papaya",0,5.35,"no es de proximidad","Islas Canarias");
+var fresa = new FrutaVerano("fresa",0,5.74,"no es de proximidad","Malaga");
+var melocoton= new FrutaVerano("melocoton",0,1.40,"no es de proximidad","Murcia");
+
 
 
 /**
